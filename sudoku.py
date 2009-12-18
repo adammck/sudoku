4c1b6eca2040deaeedbdcf9d38b77a6f08cc8c24
adammck <adam.mckaig@gmail.com>
2009-12-18 14:26:54 -0500

    fleshed out Cell.

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,32 @@
 class Cell(object):
     """
     Cells contain either None, or an integer 0-9.
+
+    >>> Cell(None)
+    <Cell:?>
+
+    >>> Cell(5)
+    <Cell:5>
+
+    Attempting to store anything else results in a ValueError.
+
+    >>> Cell(99)
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid value: 99
     """
+
+    def __init__(self, value=None):
+        if (value is None) or ((type(value) is int) and (0 <= value <= 9)):
+            self.value = value
+
+        else:
+            raise ValueError(
+                "Invalid value: %r" % value)
+
+    def __repr__(self):
+        return ("<Cell:%s>" %
+            (self.value if (self.value is not None) else "?"))
 
 
 class Column(object):
