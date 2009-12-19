0ac08bdf7cf2bec5284d6738ac2c59dc105a6391
adammck <adam.mckaig@gmail.com>
2009-12-19 07:52:17 -0500

    removed doctests.
    i'm more interested in the terseness of this solution that testing
    every single component. wooo unreliable software.

--- generator.py
+++ generator.py
@@ -55,27 +55,10 @@ def render_board(board):
 def swap_row(board, a, b):
     """
     Swap in place two rows of *board*.
-
-    >>> brd = parse_board(COMPLETE)
-    >>> swap_row(brd, 0, 1)
-    >>> swap_row(brd, 7, 8)
-    >>> print render_board(brd)
-    456|789|123
-    123|456|789
-    789|123|456
-    ---+---+---
-    234|567|891
-    567|891|234
-    891|234|567
-    ---+---+---
-    345|678|912
-    912|345|678
-    678|912|345
     """
 
     sa = slice((a*9), (a+1)*9)
     sb = slice((b*9), (b+1)*9)
-    #print "R%d > R%d" % (a, b)
 
     board[sa], board[sb] =\
         board[sb], board[sa]
@@ -91,27 +74,10 @@ def shuffle_row(board):
 def swap_column(board, a, b):
     """
     Swap in place two columns of *board*.
-
-    >>> brd = parse_board(COMPLETE)
-    >>> swap_column(brd, 0, 1)
-    >>> swap_column(brd, 7, 8)
-    >>> print render_board(brd)
-    213|456|798
-    546|789|132
-    879|123|465
-    ---+---+---
-    324|567|819
-    657|891|243
-    981|234|576
-    ---+---+---
-    435|678|921
-    768|912|354
-    192|345|687
     """
 
     sa = slice(a, 81+a, 9)
     sb = slice(b, 81+b, 9)
-    #print "C%d > C%d" % (a, b)
 
     board[sa], board[sb] =\
         board[sb], board[sa]
@@ -124,17 +90,13 @@ def shuffle_column(board):
     sw = random.choice(SWAPS)
     swap_column(board, *sw)
 
-board = parse_board(COMPLETE)
-
-shuffles = (
-    shuffle_row,
-    shuffle_column)
-
-for n in range(999):
-    random.choice(shuffles)(board)
-
-print render_board(board)
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    board = parse_board(COMPLETE)
+
+    for n in range(999):
+        random.choice([
+            shuffle_row,
+            shuffle_column])(board)
+
+    print render_board(board)
